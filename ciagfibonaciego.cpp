65b31485c75e136ec68d39b3e401b9f5b584de37
masiukiev <98082151+masiukiev@users.noreply.github.com>
2022-04-11 14:46:11 +0200

    Add files via upload
    kod z ciagiem fibonaciego ;) działa, to jest sukces

---
+++ ciagfibonaciego.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 0;
+	int b = 1;
+	int moves = 0;
+	cout << "ile razy wypisać ?\n";
+	cin >> moves;
+	for (int i = 0; i < moves; i++)
+	{
+		if (a > b) {
+			b = a + b;
+			cout << b << endl;
+		}
+		else {
+			a = a + b;
+			cout << a << endl;
+		}
+	}
+}
