ede7d4896da606906bd78d541fba35312e40eacc
masiukiev <98082151+masiukiev@users.noreply.github.com>
2022-05-09 14:33:36 +0200

    Add files via upload

---
+++ liczbypierwsze.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+int main()
+{
+
+    int n = 0;
+    cout << "Ile liczb pierwszych wyświetlić?" << endl;
+    cin >> n;
+    auto start = high_resolution_clock::now();
+    int p = 2;
+    cout << "Liczby pierwsze:" << endl;
+    for (int c = 0; c < n; c)
+    {
+        bool pier = true;
+        for (int i = 2; i <= p / 2; i++)
+        {
+            if (p % i == 0)
+            {
+                pier = false;
+                break;
+            }
+        }
+        if (pier == true)
+        {
+            cout << p << endl;
+            c++;
+        }
+        p++;
+
+    }
+    
+}
