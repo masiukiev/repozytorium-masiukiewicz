c1e8e9f22df998b9ad9bd7c7ed7a5e4efca92c21
masiukiev <98082151+masiukiev@users.noreply.github.com>
2022-05-09 14:41:45 +0200

    Add files via upload

---
+++ nnw_i_nwd.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cout << "Podaj dwie liczby\n";
+    cin >> a >> b;
+    int iloczyn = a * b;
+    while (a != b)
+    {
+        if (a <= b)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+    }
+    float nww = iloczyn / a;
+    cout << "NWD = " << a << endl;
+    cout << "NWW = " << nww;
+    
+}
